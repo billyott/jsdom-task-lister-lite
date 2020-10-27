81d83d5e4bfc39cda80fa906d0b8841c5e8926dc
billyott <willrichott@gmail.com>
2020-10-27 16:38:54 -0400

    Done! YAY

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const todoList = document.querySelector("#tasks")
+  const submitButton = document.querySelector("[type='submit']")
+  submitButton.addEventListener('click', function(event) {
+    const newTask = document.createElement("li");
+    newTask.innerHTML = document.querySelector("#new-task-description").value
+    todoList.append(newTask)
+    event.preventDefault();
+  });
 });
